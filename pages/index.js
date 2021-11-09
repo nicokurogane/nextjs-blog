2cc6ac5b17899eac21a11e75b73d9ee06098e5af
Nicolas Martinez <fyedream@gmail.com>
2021-11-08 18:52:57 -0600

    🐛 link import bugfix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Link from "next/Link";
+import Link from "next/link";
 import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
